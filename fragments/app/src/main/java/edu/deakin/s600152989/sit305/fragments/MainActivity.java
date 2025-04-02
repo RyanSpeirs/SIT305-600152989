4c1c448102525cdaeeafd2697c1ab83a7722b17f
RyanSpeirs <61259710+RyanSpeirs@users.noreply.github.com>
2025-04-02 18:54:44 +1100

    Fragments assignment
    will rename this later

--- fragments/app/src/main/java/edu/deakin/s600152989/sit305/fragments/MainActivity.java
+++ fragments/app/src/main/java/edu/deakin/s600152989/sit305/fragments/MainActivity.java
@@ -1,24 +1,47 @@
 package edu.deakin.s600152989.sit305.fragments;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnburwood, btngeelong;
+    View mainView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainView), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    public void selectFragment(View view) {
+        Fragment fragment;
+
+        if(view.getId() == R.id.btnburwood) {
+            fragment = new BurwoodFragment();
+        } else if (view.getId()== R.id.btngeelong){
+            fragment = new GeelongFragment();
+
+        } else {
+            throw new RuntimeException("wrong thing happened");
+        }
+    };
 }
+
+
+
+
